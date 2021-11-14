a427dee943f10b51d65597345a3746f825db91fe
Dasha <dbarkhatova7@gmail.com>
2021-11-14 19:51:34 +0300

    add new mesage

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ bot = telebot.TeleBot('1655026971:AAE5-t08Tdvqv9-3pXXuL8EFV5_INvxZnzc')
 
 @bot.message_handler(content_types=['text'])
 def handle_text_messages(message):
-    if message.text == "Привет":
+    if message.text == "Привет!":
         bot.send_message(message.from_user.id, "Привет")
-    elif message.text == "Кто ты?":
+    elif message.text == "Кто ты?:)":
         bot.send_message(message.from_user.id, "Я тестовый чатбот для учебного примера.")
     elif message.text == "Как тебя зовут?":
         bot.send_message(message.from_user.id, "Меня зовут MyFirstTestBot.")
+    elif message.text == "Сколько тебе лет?":
+        bot.send_message(message.from_user.id, "Мне 14")
     else:
         bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши что-то другое")
 
