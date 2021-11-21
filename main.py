187447ef3dc0497a01861e41083b636e24e797d7
Dasha <dbarkhatova7@gmail.com>
2021-11-21 20:23:25 +0300

    realno lychshiy

--- main.py
+++ main.py
@@ -1,20 +1,50 @@
-import telebot
+from bs4 import BeautifulSoup
+import requests
+from aiogram import Bot, Dispatcher, executor, types
+from config import TOKEN
+import service as s
+
+bot = Bot(token=TOKEN, parse_mode="HTML")
+
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands="start")
+async def cmd_random(message: types.Message):
+    keyboard = types.InlineKeyboardMarkup()
+    keyboard.add(types.InlineKeyboardButton(text="Получить прогноз", callback_data="get_data"))
+    keyboard.add(types.InlineKeyboardButton(text="Нет,на меня", callback_data="button2"))
+    await message.answer("Нажми на одну из нас!", reply_markup=keyboard)
+
+@dp.callback_query_handler(text="get_data")
+async def buttonAnswer(call: types.CallbackQuery):
+    data = await get_data()
+
+    resp = ''
+
+    for key, value in data.items():
+        resp += '<b>{}</b>, {}\n'.format(key, value)
+
+    await call.message.answer(resp)
+    await call.answer(text="Спасибо за ответ", show_alert=False)
+
+
+#import telebot
 #Важный коментарий
-bot = telebot.TeleBot('1655026971:AAE5-t08Tdvqv9-3pXXuL8EFV5_INvxZnzc')
+#bot = telebot.TeleBot('1655026971:AAE5-t08Tdvqv9-3pXXuL8EFV5_INvxZnzc')
 
 
-@bot.message_handler(content_types=['text'])
-def handle_text_messages(message):
-    if message.text == "Привет!":
-        bot.send_message(message.from_user.id, "Привет")
-    elif message.text == "Кто ты?:)":
-        bot.send_message(message.from_user.id, "Я тестовый чатбот для учебного примера")
-    elif message.text == "Как тебя зовут?":
-        bot.send_message(message.from_user.id, "Меня зовут MyFirstTestBot.")
-    elif message.text == "Сколько тебе лет?":
-        bot.send_message(message.from_user.id, "Мне лень отвечать")
-    else:
-        bot.send_message(message.from_user.id, "Эээээ... Что? Я не понимаю((((")
+#@bot.message_handler(content_types=['text'])
+#def handle_text_messages(message):
+#    if message.text == "Привет!":
+#        bot.send_message(message.from_user.id, "Привет")
+#    elif message.text == "Кто ты?:)":
+#        bot.send_message(message.from_user.id, "Я тестовый чатбот для учебного примера")
+#    elif message.text == "Как тебя зовут?":
+#        bot.send_message(message.from_user.id, "Меня зовут MyFirstTestBot.")
+#    elif message.text == "Сколько тебе лет?":
+#        bot.send_message(message.from_user.id, "Мне лень отвечать")
+#    else:
+#        bot.send_message(message.from_user.id, "Эээээ... Что? Я не понимаю((((")
 
 
-bot.polling(none_stop=True, interval=0)
+#bot.polling(none_stop=True, interval=0)
---
+++ service.py
@@ -0,0 +1,23 @@
+from bs4 import BeautifulSoup
+import requests
+
+async def get_data():
+    url = "https://www.meteonova.ru/allergy/27553-Nizhniy_Novgorod.htm"
+    result = requests.get(url, headers={
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0'})
+
+    soup = BeautifulSoup(result.text, 'html.parser')
+
+    allergy_div = soup.find("div", {"id": "frc_content_weather"})
+    table = allergy_div.find("table")
+
+    attributes = dict()
+
+    for row in table.find_all('tr'):
+        columns = row.find_all('td')
+        if len(columns) > 0:
+            attributes[columns[0].get_text()] = columns[len(columns)-1].get_text()
+
+    return attributes
+
+executor.start_polling(dp, skip_updates=True)
