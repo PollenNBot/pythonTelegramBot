a7d390e4c287f2c002f848a4a145a4c91e626b06
artemya <artem.a.malyshev.2008@gmail.com>
2021-11-21 20:38:53 +0300

    samiy luchshiy bot

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from bs4 import BeautifulSoup
-import requests
 from aiogram import Bot, Dispatcher, executor, types
 from config import TOKEN
 import service as s
@@ -17,7 +15,7 @@ async def cmd_random(message: types.Message):
 
 @dp.callback_query_handler(text="get_data")
 async def buttonAnswer(call: types.CallbackQuery):
-    data = await get_data()
+    data = await s.get_data()
 
     resp = ''
 
@@ -28,23 +26,4 @@ async def buttonAnswer(call: types.CallbackQuery):
     await call.answer(text="Спасибо за ответ", show_alert=False)
 
 
-#import telebot
-#Важный коментарий
-#bot = telebot.TeleBot('1655026971:AAE5-t08Tdvqv9-3pXXuL8EFV5_INvxZnzc')
-
-
-#@bot.message_handler(content_types=['text'])
-#def handle_text_messages(message):
-#    if message.text == "Привет!":
-#        bot.send_message(message.from_user.id, "Привет")
-#    elif message.text == "Кто ты?:)":
-#        bot.send_message(message.from_user.id, "Я тестовый чатбот для учебного примера")
-#    elif message.text == "Как тебя зовут?":
-#        bot.send_message(message.from_user.id, "Меня зовут MyFirstTestBot.")
-#    elif message.text == "Сколько тебе лет?":
-#        bot.send_message(message.from_user.id, "Мне лень отвечать")
-#    else:
-#        bot.send_message(message.from_user.id, "Эээээ... Что? Я не понимаю((((")
-
-
-#bot.polling(none_stop=True, interval=0)
+bot.polling(none_stop=True, interval=0)
